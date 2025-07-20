32ada93a0f427da218f7877e38533ea177f7e3b5
Matej <hozlarmatej0@gmail.com>
2025-07-20 23:14:48 +0200

    styles tweaks

--- client/dist-electron/electron.js
+++ client/dist-electron/electron.js
@@ -6,22 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let mainWindow = null;
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-  mainWindow.setMenuBarVisibility(false);
-  mainWindow.removeMenu();
-  const url = isDev
-    ? "http://localhost:5173"
-    : `file://${path.join(__dirname, "../dist/index.html")}`;
-  mainWindow.loadURL(url);
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    mainWindow.setMenuBarVisibility(false);
+    mainWindow.removeMenu();
+    const url = isDev
+        ? "http://localhost:5173"
+        : `file://${path.join(__dirname, "../dist/index.html")}`;
+    mainWindow.loadURL(url);
 };
 app.whenReady().then(createWindow);
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+    if (process.platform !== "darwin")
+        app.quit();
 });
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,43 @@
 import logo from "../assets/logo/saunhardy-logo.png";
 
+type ElectronShell = {
+  openExternal: (url: string) => void;
+};
+
+type ElectronWindow = Window & {
+  electron?: {
+    shell?: ElectronShell;
+  };
+};
+
 const Header = () => {
+  const handleLogoClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const win = window as ElectronWindow;
+
+    if (win.electron?.shell?.openExternal) {
+      win.electron.shell.openExternal(
+        "https://github.com/matejhozlar/mc-server-setup-ui"
+      );
+    } else {
+      window.open(
+        "https://github.com/matejhozlar/mc-server-setup-ui",
+        "_blank"
+      );
+    }
+  };
+
   return (
     <header className="header">
       <div className="header-left">
         <h1 className="header-title">Minecraft Web Configurator</h1>
       </div>
       <div className="header-right">
-        <a href="https://github.com/matejhozlar/mc-server-setup-ui">
+        <a
+          href="https://github.com/matejhozlar/mc-server-setup-ui"
+          onClick={handleLogoClick}
+        >
           <img src={logo} alt="App Logo" className="header-logo" />
         </a>
       </div>
