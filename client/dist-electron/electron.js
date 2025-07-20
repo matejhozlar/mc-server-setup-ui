3899b359486a39bd57deaf7c33174eb9f34a0b9e
Matej <hozlarmatej0@gmail.com>
2025-07-20 23:09:27 +0200

    electron fix

--- client/dist-electron/electron.js
+++ client/dist-electron/electron.js
@@ -6,24 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let mainWindow = null;
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-    mainWindow.setMenuBarVisibility(false);
-    mainWindow.removeMenu();
-    const url = isDev
-        ? "http://localhost:5173"
-        : `file://${path.join(__dirname, "../dist/index.html")}`;
-    mainWindow.loadURL(url);
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.removeMenu();
+  const url = isDev
+    ? "http://localhost:5173"
+    : `file://${path.join(__dirname, "../dist/index.html")}`;
+  mainWindow.loadURL(url);
 };
 app.whenReady().then(createWindow);
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin")
-        app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
